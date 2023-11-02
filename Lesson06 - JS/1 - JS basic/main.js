7efaecef6d8db283899872522e0615ab26bc9a4a
Bayasgalan Battulga <b.bayasgalan0912@gmail.com>
2023-11-02 14:04:51 +0800

    update

---
+++ Lesson06 - JS/1 - JS basic/main.js
@@ -0,0 +1,7 @@
+let a = 200;
+let b = 500;
+console.log('a=' + a + ' b=' + b)
+let c = a;
+a = b;
+b = c;
+console.log('a=' + a + ' b=' + b)
---
+++ Lesson06 - JS/2 - JS if-else/main.js
@@ -0,0 +1,20 @@
+
+
+
+let temperature = 36;
+
+if (temperature > 30 ) {
+    console.log("Шорт фодволк өмсье");
+  } else if (temperature <= 30 && temperature > 20 ) {
+    console.log("Нимгэн цамц авчъя");
+  } else if (temperature <= 20 && temperature > 10) {
+    console.log("Өмд цамц өмсье");
+  } else {
+    console.log("Гадуур хувцас өмсье");
+  }
+
+  if (temperature >30) {
+    if(temperature <40) {
+        console
+    }
+  }
